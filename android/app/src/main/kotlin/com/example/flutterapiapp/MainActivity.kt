df7b891995c23a3928ab10c7c212108956a83f77
Vandita Chapadia <vanditachapadia296@gmail.com>
2021-04-04 23:10:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutterapiapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterapiapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapiapp/screens/tracker.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Tracker(),
+    );
+  }
+}
